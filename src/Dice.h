819d8623f40df7a5eafeee267bd119f66ccbec86
Owen Wang <owenwang0099@gmail.com>
2025-03-29 20:00:22 -0400

    Added dice functions

--- src/Dice.h
+++ src/Dice.h
@@ -1,18 +1,21 @@
 #ifndef DICE_H
 #define DICE_H
 
+#include <random>
 #include <vector>
 
 class Dice {
 public:
-  Dice(int numSides);
+  Dice() = delete;
 
-  void setSides(int n);
+  static std::vector<int> roll(int sides, int n);
 
-  std::vector<int> roll(int n) const;
+  static bool getProbability(double p);
+
+  static int getDiscreteProbability(const std::vector<double> &probabilities);
 
 private:
-  int numSides;
+  static std::mt19937 gen;
 };
 
 #endif
