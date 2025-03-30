06d96c2cbce7315283ab1a2fc86e7efe2e566a1e
Owen Wang <owenwang0099@gmail.com>
2025-03-29 20:08:53 -0400

    Added cup limit

--- src/Board.h
+++ src/Board.h
@@ -11,16 +11,21 @@ class Board {
 public:
   Board();
 
+  constexpr static const int MAX_CUPS = 4;
+
   void displayBoard() const;
 
-  std::string printImprovements(std::shared_ptr<AcademicBuilding> building) const;
+  std::string
+  printImprovements(std::shared_ptr<AcademicBuilding> building) const;
 
-  std::string printEmptySquareWithPieces(std::shared_ptr<Building> building) const;
+  std::string
+  printEmptySquareWithPieces(std::shared_ptr<Building> building) const;
 
   const std::vector<std::shared_ptr<Building>> &getBuildings() const;
 
 private:
   std::vector<std::shared_ptr<Building>> buildings;
+  std::shared_ptr<int> active_cups;
 };
 
 #endif
