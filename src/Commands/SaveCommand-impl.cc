c5bb43037d53d0a1de712fa6451ed1de3c6bee40
Owen Wang <owenwang0099@gmail.com>
2025-03-30 18:46:56 -0400

    minor bugfix

--- src/Commands/SaveCommand-impl.cc
+++ src/Commands/SaveCommand-impl.cc
@@ -1,95 +1,84 @@
+#include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream> 
 
+#include "../Buildings/AcademicBuilding.h"
+#include "../Buildings/Building.h"
+#include "../Buildings/GymBuilding.h"
+#include "../Buildings/ResidenceBuilding.h"
 #include "CommandContext.h"
 #include "SaveCommand.h"
-#include "../Buildings/Building.h"
-#include "../Buildings/AcademicBuilding.h"
-#include "../Buildings/ResidenceBuilding.h"
-#include "../Buildings/GymBuilding.h"
 
 const std::string SaveCommand::NAME = "save";
 
-SaveCommand::SaveCommand(std::weak_ptr<CommandContext> context)
+SaveCommand::SaveCommand(std::shared_ptr<CommandContext> context)
     : Command{context} {}
 
 void SaveCommand::execute(const std::vector<std::string> &params) {
-    auto ctx = context.lock();
-    if (!ctx) {
-        return;
-    }
-    std::string filename = params[0];
-    if (!filename.ends_with(".txt")) {
-        filename += ".txt";
-    }
-    std::ofstream savefile(filename);
+  std::string filename = params[0];
+  if (!filename.ends_with(".txt")) {
+    filename += ".txt";
+  }
+  std::ofstream savefile(filename);
 
-    savefile << ctx->players.size() << std::endl;
+  savefile << context->players.size() << std::endl;
 
-    for (int i = ctx->cur_player_idx; i < ctx->players.size(); i++) {
-        auto player = ctx->players[i];
-        savefile << player->getName() << " " << player->getPiece() << " " << player->getCups() << " "
-                 << player->getBalance() << " " << player->getPosition();
-        if (player->getPosition() == 10) {
-            if (player->getTurnsInTims()) {
-                savefile << "1 " << player->getTurnsInTims() - 1 << "\n";
-            } else {
-                savefile << " 0";
-            }
-        }
+  for (int i = context->cur_player_idx; i < context->players.size(); i++) {
+    auto player = context->players[i];
+    savefile << player->getName() << " " << player->getPiece() << " "
+             << player->getCups() << " " << player->getBalance() << " "
+             << player->getPosition();
+    if (player->getPosition() == 10) {
+      if (player->getTurnsInTims()) {
+        savefile << "1 " << player->getTurnsInTims() - 1 << "\n";
+      } else {
+        savefile << " 0";
+      }
     }
-    for (int i = 0; i < ctx->cur_player_idx; i++) {
-        auto player = ctx->players[i];
-        savefile << player->getName() << " " << player->getPiece() << " " << player->getCups() << " "
-                 << player->getBalance() << " " << player->getPosition();
-        
-        if (player->getPosition() == 10) {
-            if (player->getTurnsInTims()) {
-                savefile << "1 " << player->getTurnsInTims() - 1 << "\n";
-            } else {
-                savefile << " 0";
-            }
-        }
-        savefile << "\n";
-    }
-    // for (auto &player : ctx->players) {
-    //     savefile << player->getName() << " " << player->getPiece() << " " << player->getCups() << " "
-    //              << player->getBalance() << " " << player->getPosition();
-    //     if (player->getTurnsInTims()) {
-    //         savefile << "1 " << player->getTurnsInTims() - 1 << "\n";
-    //     } else {
-    //         savefile << " 0\n";
-    //     }
-    // }
+  }
+  for (int i = 0; i < context->cur_player_idx; i++) {
+    auto player = context->players[i];
+    savefile << player->getName() << " " << player->getPiece() << " "
+             << player->getCups() << " " << player->getBalance() << " "
+             << player->getPosition();
 
-    for (auto building : ctx->board->getBuildings()) {
-        if (auto b = std::dynamic_pointer_cast<AcademicBuilding>(building)) {
-            savefile << b->getName() << " "; 
-            if (b->hasOwner()) {
-                savefile << b->getOwner()->getName() << " ";
-            } else {
-                savefile << "BANK ";
-            } 
-            savefile << b->getNumImprovements() << std::endl;
-        } else if (auto b = std::dynamic_pointer_cast<ResidenceBuilding>(building)) {
-            savefile << b->getName() << " ";
-            if (b->hasOwner()) {
-                savefile << b->getOwner()->getName() << " ";
-            } else {
-                savefile << "BANK ";
-            }  
-            savefile << "0" << std::endl;
-        } else if (auto b = std::dynamic_pointer_cast<GymBuilding>(building)) {
-            savefile << b->getName() << " ";
-            if (b->hasOwner()) {
-                savefile << b->getOwner()->getName() << " ";
-            } else {
-                savefile << "BANK ";
-            }  
-            savefile << "0\n";
-        }
+    if (player->getPosition() == 10) {
+      if (player->getTurnsInTims()) {
+        savefile << "1 " << player->getTurnsInTims() - 1 << "\n";
+      } else {
+        savefile << " 0";
+      }
     }
-    savefile.close();
+    savefile << "\n";
+  }
+
+  for (auto &building : context->board->getBuildings()) {
+    if (auto b = std::dynamic_pointer_cast<AcademicBuilding>(building)) {
+      savefile << b->getName() << " ";
+      if (b->hasOwner()) {
+        savefile << b->getOwner()->getName() << " ";
+      } else {
+        savefile << "BANK ";
+      }
+      savefile << b->getNumImprovements() << std::endl;
+    } else if (auto b =
+                   std::dynamic_pointer_cast<ResidenceBuilding>(building)) {
+      savefile << b->getName() << " ";
+      if (b->hasOwner()) {
+        savefile << b->getOwner()->getName() << " ";
+      } else {
+        savefile << "BANK ";
+      }
+      savefile << "0" << std::endl;
+    } else if (auto b = std::dynamic_pointer_cast<GymBuilding>(building)) {
+      savefile << b->getName() << " ";
+      if (b->hasOwner()) {
+        savefile << b->getOwner()->getName() << " ";
+      } else {
+        savefile << "BANK ";
+      }
+      savefile << "0\n";
+    }
+  }
+  savefile.close();
 }
-
--- src/Commands/SaveCommand.h
+++ src/Commands/SaveCommand.h
@@ -7,7 +7,7 @@ class SaveCommand : public Command {
 public:
   static const std::string NAME;
 
-  explicit SaveCommand(std::weak_ptr<CommandContext> context);
+  explicit SaveCommand(std::shared_ptr<CommandContext> context);
 
   void execute(const std::vector<std::string> &params) override;
 };
