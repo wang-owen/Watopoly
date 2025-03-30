00675b009e69409147ef0c233e94274c5e5e1f7a
Owen Wang <owenwang0099@gmail.com>
2025-03-29 22:32:03 -0400

    Added Goose Nesting event

--- src/Buildings/GooseNesting.h
+++ src/Buildings/GooseNesting.h
@@ -1,13 +1,21 @@
 #ifndef GOOSENESTING_H
 #define GOOSENESTING_H
 
+#include "Player.h"
 #include "UnownableBuilding.h"
 
 class GooseNesting : public UnownableBuilding {
 public:
   GooseNesting() : UnownableBuilding{"GOOSE NESTING"} {}
-  
-  void processEvent(const std::shared_ptr<Player> &player) {}
+
+  void processEvent(const std::shared_ptr<Player> &player) {
+    std::cout << player->getName()
+              << " has disturbed a flock of nesting geese!\n";
+    std::cout << "The geese honk angrily and flap their wings, but do no real "
+                 "harm.\n";
+    std::cout << player->getName()
+              << " escapes unscathed, but slightly shaken.\n";
+  }
 };
 
 #endif
