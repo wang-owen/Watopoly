8b1cb6fbc54d743627601e88d6e3d0148e26775f
Owen Wang <owenwang0099@gmail.com>
2025-03-29 22:32:07 -0400

    Added Tuition event

--- src/Buildings/Tuition.h
+++ src/Buildings/Tuition.h
@@ -5,9 +5,12 @@
 
 class Tuition : public UnownableBuilding {
 public:
-  Tuition() : UnownableBuilding{"TUITION"} {}
+  Tuition();
 
-  void processEvent(const std::shared_ptr<Player> &player) override {}
+  void processEvent(const std::shared_ptr<Player> &player) override;
+private:
+  const int TUITION_AMOUNT;
+  const double PERCENT_AMOUNT;
 };
 
 #endif
