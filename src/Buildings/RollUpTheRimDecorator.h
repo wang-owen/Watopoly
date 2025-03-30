bbc57944cae83805edbd1756faa23aba2dd092fd
Owen Wang <owenwang0099@gmail.com>
2025-03-29 20:07:41 -0400

    Added Roll Up The Rim functionality

--- src/Buildings/RollUpTheRimDecorator.h
+++ src/Buildings/RollUpTheRimDecorator.h
@@ -5,8 +5,15 @@
 
 class RollUpTheRimDecorator : public UnownableBuilding {
 public:
-  RollUpTheRimDecorator(std::shared_ptr<UnownableBuilding> building)
-      : UnownableBuilding{building->getName()} {}
+  RollUpTheRimDecorator(std::unique_ptr<UnownableBuilding> building,
+                        std::shared_ptr<int> active_cups);
+
+  void processEvent(const std::shared_ptr<Player> &player) override;
+
+private:
+  std::unique_ptr<UnownableBuilding> building;
+  const double probability = 0.01;
+  std::shared_ptr<int> active_cups;
 };
 
 #endif
