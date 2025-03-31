d68b29b594681659b4e5ed899c6abfd3d7456246
Xuan Zheng <eagledude200@gmail.com>
2025-03-31 11:35:17 -0400

    minor fix

--- src/Commands/AllCommand-impl.cc
+++ src/Commands/AllCommand-impl.cc
@@ -11,6 +11,9 @@ AllCommand::AllCommand(std::shared_ptr<CommandContext> context)
 
 void AllCommand::execute(const std::vector<std::string> & /*params*/) {
   for (auto &player : context->players) {
+    if (!player->isActive()) {
+      continue;
+    }
     player->displayAssets();
   }
 }
